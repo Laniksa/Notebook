109bdca554a7ce004c4727bfecfa81a3a36480fe
Регина <rmashkovtseva@gmail.com>
2023-03-20 14:58:29 +0300

    изменила меню

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,41 +5,44 @@ import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Main {
-    Scanner scan = new Scanner(System.in);
+    public static Scanner scan = new Scanner(System.in);
     public static ArrayList<Task> task = new ArrayList<>();
 
-    public static boolean menu(Scanner scan){
-        while(true){
-            System.out.println("Выберите действие:\n" +
-                    "1. Добавить задачу\n" +
-                    "2. Вывести список задач\n" +
-                    "3. Удалить задачу\n" +
-                    "0. Выход");
-            int num = scan.nextInt();
-            if(num == 1){
+    public static boolean menu(Scanner scan) {
+        System.out.println("Выберите действие:\n" +
+                "1. Добавить задачу\n" +
+                "2. Вывести список задач\n" +
+                "3. Удалить задачу\n" +
+                "0. Выход");
+
+        switch (scan.nextInt()) {
+            case 1:
                 System.out.println("Введите задачу: ");
                 String str = scan.nextLine();
                 task.add(new Task(str));
-            }if(num == 2){
+                break;
+            case 2:
                 System.out.println(task.toString());
-            }if (num ==3){
+                break;
+            case 3:
                 System.out.println("Введите номер задачи для удаления \n");
                 System.out.println(task.toString());
                 int numTask = scan.nextInt();
-                task.remove(num-1);
+                task.remove(numTask - 1);
                 System.out.println("Оставшиеся задачи: \n");
                 System.out.println(task.toString());
-            }if (num == 0){
+                break;
+            case 4:
                 System.out.println("Программа завершена");
-                return true;
-            }else{
+                break;
+            default:
                 System.out.println("Вы ввелм неверное значение");
-            }
+                break;
         }
-
+        return true;
     }
 
     public static void main(String[] args) {
-
+    menu(scan);
     }
 }
